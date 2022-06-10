1a625e9c42c0892facaef4030ab4e50449cc9ddf
obregonscott <obregon.scott@gmail.com>
2022-06-10 18:01:21 -0500

    testing

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -20,19 +20,45 @@ const generateBadge = License => {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
+let renderLicenseLink = license => {
+ if (license === 'Apache') {
+  return `Apache
+Copyright (c) ${new Date().getFullYear()}
 
-//function renderLicenseLink(license) { }
+for more information about Apache license click [Link](https://opensource.org/licenses/Apache-2.0)`
+} else if (license === 'BSD 3-clause') {
+  return `BSD 3-clause
+Copyright (c) ${new Date().getFullYear()}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+for more information about BSD 3-clause license click [Link](https://opensource.org/licenses/BSD-3-Clause)`
+} else if (license === 'GPLv2') {
+  return `GPLv2
+  Copyright (c) ${new Date().getFullYear()}
 
-//function renderLicenseSection(license) { }
+  for more information about GPlv2 license click [Link](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)
+  `
+} else if (license === 'ISC License'){
+  return `ISC License
+  Copyright (c) ${new Date().getFullYear()}
+
+  for more information about ISC License click [link](https://opensource.org/licenses/ISC)`
+} else if (license === 'MIT') {
+   return `MIT
+Copyright (c) ${new Date().getFullYear()}
+     
+for more information about MIT License click [link](https://opensource.org/licenses/MIT)`
+} else if (license === 'None') {
+    return `This application doesnt have any form of licensing`
+  }
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
+  # Title:
+  <h1 align="center> ${data.title} </h1>
 
-  <h1 align="center>${data.title} </h1>
+  ${generateBadge}
 
   ## Description
 
@@ -47,6 +73,8 @@ function generateMarkdown(data) {
   - [Contributing](#contributing)
   - [Author](#Author)
 
+  ###${generateBadge}
+  
   ## User Story
 
   ${data.userStory}
@@ -61,7 +89,7 @@ function generateMarkdown(data) {
 
   ## Technology
 
-  ${data.languages}
+  ${data.languages.join(', ')}
 
   ## License
 
